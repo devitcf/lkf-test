eaab38c857ce5cfdc20a0810767f48af0b8967e5
Devi Tse <devitcf@gmail.com>
2024-05-01 22:20:21 +0800

    feat: Add Customer module

---
+++ ros_backend/src/customer/customer.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, NotFoundException, ParseIntPipe } from "@nestjs/common";
+import { CustomerService } from "./customer.service";
+import { CreateCustomerDto } from "./dto/create-customer.dto";
+import { UpdateCustomerDto } from "./dto/update-customer.dto";
+
+@Controller("customers")
+export class CustomerController {
+  constructor(private readonly customerService: CustomerService) {}
+
+  @Post()
+  async create(@Body() createCustomerDto: CreateCustomerDto) {
+    return this.customerService.create(createCustomerDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.customerService.findAll();
+  }
+
+  @Get(":id")
+  async findOne(@Param("id", ParseIntPipe) id: number) {
+    return this.customerService.findOne(+id);
+  }
+
+  @Patch(":id")
+  async update(@Param("id", ParseIntPipe) id: number, @Body() updateCustomerDto: UpdateCustomerDto) {
+    return this.customerService.update(+id, updateCustomerDto);
+  }
+
+  @Delete(":id")
+  async remove(@Param("id", ParseIntPipe) id: number) {
+    const customer = await this.customerService.findOne(id);
+    if (!customer) throw new NotFoundException();
+
+    return this.customerService.remove(+id);
+  }
+}
---
+++ ros_backend/src/customer/customer.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@nestjs/common";
+import { CreateCustomerDto } from "./dto/create-customer.dto";
+import { UpdateCustomerDto } from "./dto/update-customer.dto";
+import { PrismaService } from "../prisma/prisma.service";
+
+@Injectable()
+export class CustomerService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createCustomerDto: CreateCustomerDto) {
+    // TODO: restaurantIds
+    const { name, email } = createCustomerDto;
+    return this.prisma.customer.create({ data: { name, email } });
+  }
+
+  async findAll() {
+    return this.prisma.customer.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.prisma.customer.findUnique({ where: { id } });
+  }
+
+  async update(id: number, updateCustomerDto: UpdateCustomerDto) {
+    // TODO: restaurantIds
+    const { name, email } = updateCustomerDto;
+    return this.prisma.customer.update({
+      where: { id },
+      data: { name, email },
+    });
+  }
+
+  async remove(id: number) {
+    return this.prisma.customer.delete({ where: { id } });
+  }
+}
